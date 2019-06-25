4f815606b68bb1cdf68da688bfc69ce0a0f9ee13
Bernard Baltrusaitis <bernard.baltrusaitis@contino.io>
2019-06-25 20:58:07 +1000

    rs

--- src/reed_solomon.go
+++ src/reed_solomon.go
@@ -1,99 +1,62 @@
 package main
 
 type Encoder interface {
-	getResult(count int) uint
-	initGF(poly uint)
-	initCode(nsym uint, index uint)
-	encode(len uint, data []uint)
+	encode(data []uint)
 }
 
 type ReedSolomon struct {
-	logmod uint
-	rlen   uint
-	logt   []uint
-	alog   []uint
-	rspoly []uint
-	res    []uint
+	log         []uint
+	alog        []uint
+	polynomials []uint
+	polynomial  uint
+	gf          uint
 }
 
 func NewReedSolomon() *ReedSolomon {
-	return &ReedSolomon{}
-}
-func (r *ReedSolomon) getResult(count int) uint {
-	return r.res[count]
-}
-
-func (r *ReedSolomon) initGF(poly uint) {
-
-	var b, p, m, v uint
-	// Find the top bit, and hence the symbol size
-	m = 0
-	for b = 1; b <= poly; b <<= 1 {
-		m++
-	}
-	b >>= 1
-	m--
-
-	// Calculate the log/alog tables
-	r.logmod = (1 << m) - 1
-	r.logt = make([]uint, r.logmod+1)
-	r.alog = make([]uint, r.logmod)
-
-	p = 1
-	for v = 0; v < r.logmod; v++ {
-		r.alog[v] = p
-		r.logt[p] = v
-		p <<= 1
-		if (p & b) != 0 {
-			p ^= poly
-		}
+	return &ReedSolomon{
+		log: []uint{
+			0, 0, 1, 6, 2, 12, 7, 26, 3, 32, 13, 35, 8, 48, 27, 18, 4, 24, 33, 16, 14, 52, 36, 54, 9, 45, 49, 38, 28,
+			41, 19, 56, 5, 62, 25, 11, 34, 31, 17, 47, 15, 23, 53, 51, 37, 44, 55, 40, 10, 61, 46, 30, 50, 22, 39,
+			43, 29, 60, 42, 21, 20, 59, 57, 58,
+		},
+		alog: []uint{
+			1, 2, 4, 8, 16, 32, 3, 6, 12, 24, 48, 35, 5, 10, 20, 40, 19, 38, 15, 30, 60, 59, 53, 41, 17, 34, 7, 14,
+			28, 56, 51, 37, 9, 18, 36, 11, 22, 44, 27, 54, 47, 29, 58, 55, 45, 25, 50, 39, 13, 26, 52, 43, 21, 42,
+			23, 46, 31, 62, 63, 61, 57, 49, 33,
+		},
+		polynomials: []uint{48, 17, 29, 30, 1},
+		polynomial:  67,
+		gf:          64,
 	}
 }
 
-func (r *ReedSolomon) initCode(nsym uint, index uint) {
-	var k, i uint
+func (r *ReedSolomon) encode(data []uint) []uint {
+	var i, k, m, l uint
 
-	r.rspoly = make([]uint, nsym+1)
+	v := make([]uint, 4)
 
-	r.rlen = nsym
-
-	r.rspoly[0] = 1
-	for i = 1; i <= nsym; i++ {
-		r.rspoly[i] = 1
-		for k = i - 1; k > 0; k-- {
-			if r.rspoly[k] != 0 {
-				r.rspoly[k] = r.alog[(r.logt[r.rspoly[k]]+index)%r.logmod]
-			}
-			r.rspoly[k] ^= r.rspoly[k-1]
-		}
-		r.rspoly[0] = r.alog[(r.logt[r.rspoly[0]]+index)%r.logmod]
-		index++
-	}
-}
-
-func (r *ReedSolomon) encode(len uint, data []uint) {
-	var i, k, m uint
-
-	r.res = make([]uint, r.rlen)
-
-	for i = 0; i < r.rlen; i++ {
-		r.res[i] = 0
+	for i = 0; i < 4; i++ {
+		v[i] = 0
 	}
 
-	for i = 0; i < len; i++ {
-		m = r.res[r.rlen-1] ^ data[i]
-		for k = r.rlen - 1; k > 0; k-- {
-			if (m != 0) && (r.rspoly[k] != 0) {
-				r.res[k] = r.res[k-1] ^ r.alog[(r.logt[m]+r.logt[r.rspoly[k]])%r.logmod]
+	l = uint(len(data))
+
+	for i = 0; i < l; i++ {
+		m = v[3] ^ data[i]
+		for k = 3; k > 0; k-- {
+			if (m != 0) && (r.polynomials[k] != 0) {
+				v[k] = v[k-1] ^ r.alog[(r.log[m]+r.log[r.polynomials[k]])%(r.gf-1)]
 			} else {
-				r.res[k] = r.res[k-1]
+				v[k] = v[k-1]
 			}
 		}
 
-		if (m != 0) && (r.rspoly[0] != 0) {
-			r.res[0] = r.alog[(r.logt[m]+r.logt[r.rspoly[0]])%r.logmod]
+		if (m != 0) && (r.polynomials[0] != 0) {
+			v[0] = r.alog[(r.log[m]+r.log[r.polynomials[0]])%(r.gf-1)]
 		} else {
-			r.res[0] = 0
+			v[0] = 0
 		}
 	}
+
+	return v
 }
